748bf79c2563e6e7526d800646ab2468d7c0f7dd
Roland Bernard <rolli.bernard@gmail.com>
2020-09-27 18:17:14 +0200

    Added tab insertion to the code editor

--- src/ui/code-editor.js
+++ src/ui/code-editor.js
@@ -1,16 +1,29 @@
 
-import React from 'react';
+import React, { createRef } from 'react';
 
 import './code-editor.scss';
 
 export function CodeEditor(props) {
     const { value, onChange, ...others } = props;
+    const input = createRef();
     let line_count = value ? value.split('\n').length : 1;
     const myOnChange = (event) => {
         if (onChange) {
             onChange(event.target.value);
         }
     };
+    const onKeyPress = (event) => {
+        if (event.key === "Tab") {
+            let sel_start_pos = event.currentTarget.selectionStart;
+            let sel_end_pos = event.currentTarget.selectionEnd;
+            let new_value = value.substring(0, sel_start_pos) + "\t" + value.substring(sel_end_pos);
+            event.preventDefault();
+            input.current.value = new_value;
+            input.current.selectionStart = sel_start_pos + 1;
+            input.current.selectionEnd = sel_start_pos + 1;
+            onChange(new_value);
+        }
+    }
     return (
         <div className="ui-code-editor" {...others}>
             <div className="ui-code-editor-container">
@@ -20,8 +33,10 @@ export function CodeEditor(props) {
                     ))}
                 </div>
                 <textarea
+                    ref={input}
                     value={value}
                     onChange={myOnChange}
+                    onKeyDown={onKeyPress}
                     className="ui-code-editor-input"
                     placeholder="Type some code..."
                     autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false"
