eeaad8bda53503d6845774912e0946f23d577842
Roland Bernard <rolli.bernard@gmail.com>
2020-09-27 17:38:42 +0200

    Created the paper component

---
+++ src/ui/paper.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+import './paper.scss';
+
+export function Paper(props) {
+    return (
+        <span className="ui-paper" {...props}>
+        </span>
+    );
+}
+
+export default Paper;
+
